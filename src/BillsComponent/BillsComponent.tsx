6148b3317ff7fed663bfc49034345bd41f1f3fa8
Hal Hawkins <hal.hawkins@gmail.com>
2025-02-05 13:56:30 -0500

    adding bills summary

--- src/BillsComponent/BillsComponent.tsx
+++ src/BillsComponent/BillsComponent.tsx
@@ -2,32 +2,45 @@ import { FC, useEffect, useState } from "react";
 import { Bill, fetchBillsAPI } from "./BillsAPI";
 import { useDispatch } from "react-redux";
 import { setBills } from "./BillsSlice";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+import "./BillsComponent.css";
 const BillsComponent:FC = () => {
     const [billsArray, setBillsArray] = useState<Bill[]>([]);
     const dispatch = useDispatch();
+    const bills = useSelector((state: RootState) => state.bills)
 
     useEffect(() => {
         const fetchBills = async () => {
             const billsResponse = await fetchBillsAPI(0);
             if (billsResponse) {
-                setBillsArray(billsResponse);
+                // setBillsArray(billsResponse);
                 dispatch(setBills({bills:billsResponse, page: 0}));
             }
+            // console.log("Fetched bills", billsResponse);
         };
         fetchBills();
     }, [dispatch]);
 
     useEffect(() => {
-        if (billsArray.length > 0) {
-            dispatch(setBills({                
-                bills: billsArray,
-                // bills: billsResponse,
-                page: 0
-            }));
-        }
-    },[billsArray])
+        console.log("Bills updated", bills);
+        // Update the state with the latest fetched bills
+        // setBillsArray(bills);
+        // dispatch(setBills({bills:bills, page: 0})); // Update the Redux state as well
+        // console.log("Fetched bills", bills);
+    },[bills])
     return (
         <div className="bills-list-container">
+            <ul>
+                {bills.bills.map((bill, index) => (
+                    <li key={index} className="bill-item">
+                        <div key={index} className="bill-item">
+                            <div className="bill-date">{bill.latestAction.actionDate}</div> 
+                            <span className="bill-title">{bill.title}</span>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import billsSlice from './BillsComponent/BillsSlice';
 
 export const store = configureStore({
-  reducer: {}, // You'll add your reducers here later
+  reducer: {
+    bills: billsSlice,
+  }, // You'll add your reducers here later
 });
 
 export type RootState = ReturnType<typeof store.getState>;
