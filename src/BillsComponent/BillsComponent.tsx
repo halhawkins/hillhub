c232b2f616098bdea364cc8a288623407fc1f601
Hal Hawkins <hal.hawkins@gmail.com>
2025-02-05 18:06:43 -0500

    Formatting bills output

--- src/BillsComponent/BillsComponent.tsx
+++ src/BillsComponent/BillsComponent.tsx
@@ -29,18 +29,68 @@ const BillsComponent:FC = () => {
         // dispatch(setBills({bills:bills, page: 0})); // Update the Redux state as well
         // console.log("Fetched bills", bills);
     },[bills])
+    const date = new Date();
+    const getBillType = (type: string) => {
+        switch (type) {
+            case "HR":
+                return "House Bill";
+            case "S":
+                return "Senate Bill";
+            case "HJRES":
+                return "House Joint Resolution";
+            case "SJRES":
+                return "Senate Joint Resolution";
+            case "HCONRES":
+                return "House Concurrent Resolution";
+            case "SCONRES":
+                return "Senate Concurrent Resolution";
+            case "HRES":
+                return "House Resolution";
+            case "SRES":
+                return "Senate Resolution";
+            default:
+                return "";
+        }
+    }
     return (
         <div className="bills-list-container">
-            <ul>
-                {bills.bills.map((bill, index) => (
-                    <li key={index} className="bill-item">
-                        <div key={index} className="bill-item">
-                            <div className="bill-date">{bill.latestAction.actionDate}</div> 
-                            <span className="bill-title">{bill.title}</span>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+            <table style={{width: "100%"}}>
+                <tr>
+                    <th style={{width: "7rem"}}>Date</th>
+                    <th style={{width: "5rem"}}>Chamber</th>
+                    <th style={{width: "8rem"}}>Type</th>
+                    <th>Title</th>
+                </tr>
+            </table>
+                {bills.bills.map((bill, index) => {
+                    const billDate = new Date(bill.updateDate);
+                    return (
+                        <>
+                            <table style={{width: "100%"}} key={index}>
+                                <tr>
+                                        <td style={{width: "7rem", verticalAlign: "top"}}>{billDate.toLocaleDateString('en-US', {
+                                            day: '2-digit', 
+                                            month: 'short', 
+                                            year: 'numeric'
+                                        })}</td>
+                                        <td className="bill-type" style={{width: "4rem", verticalAlign: "top"}}>
+                                            {bill.originChamber}
+                                        </td>
+                                        <td className="bill-type" style={{width: "8rem", verticalAlign: "top"}}>
+                                            {getBillType(bill.type)}
+                                        </td>
+                                    <td>
+                                        <table style={{width: "100%"}}>
+                                            <tr className="bill-title">
+                                            {bill.title.replaceAll('"',"")}
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                            <hr style={{margin: "0.5rem 0"}}/>
+                        </>
+                )})}
         </div>
     )
 }
